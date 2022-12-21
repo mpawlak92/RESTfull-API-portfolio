3e8651e628e0365542833d022b5a175cb787c94a
Michal Pawlak <mpawlak.it@gmail.com>
2022-12-21 11:30:59 +0100

    conected to Atlas MongoDB

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const URI =
-	'mongodb+srv://portfolioadmin:qwerty123456@portfoliodb.sfrdltp.mongodb.net/?retryWrites=true&w=majority';
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 
 const app = express();
+
+const URI =
+	'mongodb+srv://portfolioadmin:qwerty123456@portfoliodb.sfrdltp.mongodb.net/portfolioDB';
 
 app.set('view engine', 'ejs');
 
@@ -21,6 +21,23 @@ mongoose.set('strictQuery', true);
 mongoose.connect(URI, { useNewUrlParser: true });
 
 //create new schema
+
+const projectSchema = {
+	name: String,
+	description: String,
+	technologys: Array,
+	git_link: String,
+};
+
+const Project = mongoose.model('Project', projectSchema);
+
+app.get('/', function () {
+	Project.find(function (err, foundProjects) {
+		if (!err) {
+			console.log(foundProjects);
+		}
+	});
+});
 
 app.listen(3000, function () {
 	console.log('Server started on port 3000');
