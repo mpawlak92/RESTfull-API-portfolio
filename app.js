938f0fc0ed174dbd1502cd1e5aa17dc056631e0c
Michal Pawlak <mpawlak.it@gmail.com>
2022-12-22 14:52:02 +0100

    fix: problem with cros policy pt2

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
 const cors = require('cors');
-const corsOptions = {
-	origin: 'http://localhost:3000',
-	credentials: true, //access-control-allow-credentials:true
-	optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
 
 require('dotenv').config();
 const express = require('express');
@@ -14,6 +8,12 @@ const app = express();
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
+const corsOptions = {
+	origin: 'http://localhost:3000',
+	credentials: true, //access-control-allow-credentials:true
+	optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 app.use(
 	bodyParser.urlencoded({
 		extended: true,
