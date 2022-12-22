db52f34a86d1f3585f1f191ed0459f0f534dbe3a
Michal Pawlak <mpawlak.it@gmail.com>
2022-12-22 14:30:20 +0100

    installed cros package and add cros policy problem solution

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+const cors = require('cors');
+const corsOptions = {
+	origin: 'http://localhost:3000',
+	credentials: true, //access-control-allow-credentials:true
+	optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
