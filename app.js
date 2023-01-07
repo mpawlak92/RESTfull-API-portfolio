c4f456b9fa09d4693807908c89a2cb28e417a292
Michal Pawlak <mpawlak.it@gmail.com>
2023-01-07 21:14:44 +0100

    edited reponse meassage in patch request on aboutme route

--- app.js
+++ app.js
@@ -165,7 +165,7 @@ app
 		AboutmeData.updateOne({ $set: req.body }, function (err) {
 			if (!err) {
 				res.send({
-					info: { ...req.body },
+					info: req.body.contact,
 					message: 'Succesfully updated my datas',
 				});
 			} else {
